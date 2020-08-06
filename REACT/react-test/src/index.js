5c8af411731617cbe8421f18660952845f0be2a4
Charlie <cennis92@live.com>
2020-08-06 09:48:17 -0600

    New Cards File Created

--- REACT/react-test/src/index.js
+++ REACT/react-test/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js'
+import Cards from './Cards.js'
 import * as serviceWorker from './serviceWorker';
-import ReactCardFlip from 'react-card-flip';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Cards />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
