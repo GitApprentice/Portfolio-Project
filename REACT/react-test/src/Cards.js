88b1d5f16220a2836eca7bfcbfa09d2fb7af431e
Charlie <cennis92@live.com>
2020-08-15 12:00:42 -0600

    Outline Created For React Cards Page

--- REACT/react-test/src/Cards.js
+++ REACT/react-test/src/Cards.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Cards.css';
 import ReactCardFlip from 'react-card-flip';
 
 
@@ -19,17 +20,55 @@ class Cards extends React.Component {
    
     render() {
       return (
-        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-          <div>
-            This is the front of the card.
-            <button onClick={this.handleClick}>Click to flip</button>
-          </div>
-   
-          <div>
-            This is the back of the card.
-            <button onClick={this.handleClick}>Click to flip</button>
-          </div>
-        </ReactCardFlip>
+        <div class='outline'>    
+          <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+            <div class='front1'>
+              This is the front of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+    
+            <div class='back1'>
+              This is the back of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+          </ReactCardFlip>
+
+          <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+            <div class='front2'>
+              This is the front of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+
+            <div class='back2'>
+              This is the back of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+          </ReactCardFlip>
+
+          <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+            <div class='front3'>
+              This is the front of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+
+            <div class='back3'>
+              This is the back of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+          </ReactCardFlip>
+
+          <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+            <div class='front4'>
+              This is the front of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+
+            <div class='back4'>
+              This is the back of the card.
+              <button onClick={this.handleClick}>Click to flip</button>
+            </div>
+          </ReactCardFlip>
+        </div>  
       )
     }
   }
