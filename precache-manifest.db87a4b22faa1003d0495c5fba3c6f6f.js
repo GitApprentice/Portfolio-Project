d797c0ee7327d7585b98d5c9cbced6da31637477
Charlie <cennis92@live.com>
2020-10-14 19:07:16 -0600

    Updates

---
+++ precache-manifest.db87a4b22faa1003d0495c5fba3c6f6f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea759d150c5b58909075495040be441a",
+    "url": "/Portfolio-Project/index.html"
+  },
+  {
+    "revision": "3839d7549c77eba1e67e",
+    "url": "/Portfolio-Project/static/css/2.c0851dc3.chunk.css"
+  },
+  {
+    "revision": "73945ca9afb6f3fcc8a1",
+    "url": "/Portfolio-Project/static/css/main.676dfa49.chunk.css"
+  },
+  {
+    "revision": "3839d7549c77eba1e67e",
+    "url": "/Portfolio-Project/static/js/2.1e705ef4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio-Project/static/js/2.1e705ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73945ca9afb6f3fcc8a1",
+    "url": "/Portfolio-Project/static/js/main.6ba7cda7.chunk.js"
+  },
+  {
+    "revision": "5db932c89510c37cb9ba",
+    "url": "/Portfolio-Project/static/js/runtime-main.1eda7794.js"
+  },
+  {
+    "revision": "4a7290fca21fbf8482f27dc2c766c804",
+    "url": "/Portfolio-Project/static/media/Front End Development.4a7290fc.jpg"
+  },
+  {
+    "revision": "30b9771e819f8f2023feec3fc12b28de",
+    "url": "/Portfolio-Project/static/media/Missoula.30b9771e.jpg"
+  },
+  {
+    "revision": "6e974bf4fc6ab27b532ff300a1926c05",
+    "url": "/Portfolio-Project/static/media/Travel.6e974bf4.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Portfolio-Project/static/media/logo.ee7cd8ed.svg"
+  }
+]);
