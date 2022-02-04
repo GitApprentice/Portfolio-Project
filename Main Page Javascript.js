be140dded8e3ab0ff4c917ee392bddd812e0d934
Charlie <cennis92@live.com>
2022-02-04 10:12:33 -0700

    Home Page Redesigned

--- Main Page Javascript.js
+++ Main Page Javascript.js
@@ -1,29 +1,23 @@
-$(".About-Me").hover(function(){
-    
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav-box');
+
+menuBtn.addEventListener('click', function() {
+    menuBtn.classList.toggle('open');
+    nav.classList.toggle('open');
 })
+
+
 
 $(".About-Me").click(function(){
     window.open("About Me.html")
-})
-
-$(".My-Programming-Story").hover(function(){
-    window.open("")
 })
 
 $(".My-Programming-Story").click(function(){
     window.open("Goals.html")
 })
 
-$(".Goals").hover(function(){
-
-})
-
 $(".Goals").click(function(){
     window.open("https://gitapprentice.github.io/React-Portfolio/")
-})
-
-$(".Skills").hover(function(){
-
 })
 
 $(".Skills").click(function(){
