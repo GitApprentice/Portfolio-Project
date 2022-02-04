66de45dc914795cfedfa02b00ea44b0d6273597a
Charlie <cennis92@live.com>
2022-02-04 10:28:15 -0700

    Links Added To Home Page

--- Main Page Javascript.js
+++ Main Page Javascript.js
@@ -12,14 +12,14 @@ $(".About-Me").click(function(){
     window.open("About Me.html")
 })
 
-$(".My-Programming-Story").click(function(){
+$(".Goals").click(function(){
     window.open("Goals.html")
 })
 
-$(".Goals").click(function(){
+$(".React").click(function(){
     window.open("https://gitapprentice.github.io/React-Portfolio/")
 })
 
 $(".Skills").click(function(){
-    window.open("https://gitapprentice.github.io/Angular-Portfolio/")
+    window.open("Skills.html")
 })
